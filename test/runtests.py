64605dce31706a1d6b93fa50967e0260c9e82771
chenxiaba <chenxiaba@gmail.com>
2015-07-15 00:02:24 +0800

    Modify the testcase

--- test/runtests.py
+++ test/runtests.py
@@ -10,7 +10,18 @@ from tornado.testing import main
 
 import unittest
 
-BASE_URL = "http://localhost:8888"
+#application = tornado.web.Application([
+#		(r"/", MainHandler),
+#		(r"/api/v1/people/(.*)", PeopleHandler),
+#		(r"/api/v1/people/(.*)/ideas", UserIdeaHandler),
+#		(r"/api/v1/idea/([0-9]*)", IdeaHandler)
+#	], **settings)
+
+INDEX_URL = "http://localhost:8888"
+API_URL  = "/api/v1/"
+BASE_URL = INDEX_URL + API_URL
+
+IDEA_URL = BASE_URL + "idea"
 
 class MainTestCase(AsyncTestCase):
 	"""Test index"""
@@ -19,8 +30,31 @@ class MainTestCase(AsyncTestCase):
 	def test_index_fetch(self):
 		client = AsyncHTTPClient(self.io_loop)
 		
-		resp = yield client.fetch(BASE_URL)
+		resp = yield client.fetch(INDEX_URL)
 		self.assertIn("hello", resp.body)
+
+
+class IdeaTestCase(AsyncTestCase):
+	"""Test Idea function"""
+
+	def setUp(self):
+		print "Init IdeaTestCase..."
+		super(IdeaTestCase, self).setUp()
+		self.saved = AsyncHTTPClient._save_configuration()
+
+	def tearDown(self):
+		print "TearDown IdeaTestCase..."
+		AsyncHTTPClient._restore_configuration(self.saved)
+		super(IdeaTestCase, self).tearDown()
+
+	@gen_test(timeout=10)
+	def test_idea_add(self):
+		#Add
+		client = AsyncHTTPClient(self.io_loop)
+		
+		resp = yield client.fetch(IDEA_URL)
+
+		self.assertIn("", resp.body)
 
 
 if __name__ == '__main__':
